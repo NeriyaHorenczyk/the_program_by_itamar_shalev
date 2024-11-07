31d66b99440abe510505b0bc80fa9a77d781703d
Horenczyk Neriya <ner13579@gmail.com>
2024-11-07 17:21:55 +0200

    finished the 14 assignment and implemented the BinarySearchTree DS

---
+++ _14/BST_implementation/BinarySearchTree.h
@@ -0,0 +1,194 @@
+//
+// Created by ner13 on 07/11/2024.
+//
+
+#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+#include <iomanip>
+
+
+template<class T>
+struct Node
+{
+    T data;
+    Node* left;
+    Node* right;
+
+    explicit Node(T val) : data(val), left(nullptr), right(nullptr) {}
+
+};
+
+template<class T> class BinarySearchTree {
+
+private:
+    Node<T>* root;
+    int numOfNodes;
+
+    // Helper functions for recursive operations
+    Node<T>* insert(Node<T>* node, T value);
+    Node<T>* search(Node<T>* node, T value) const;
+    Node<T>* remove(Node<T>* node, T value);
+    Node<T>* findMin(Node<T>* node) const;
+    Node<T>* findMax(Node<T>* node) const;
+    void print(Node<T>* node, int spaces) const;
+
+public:
+    BinarySearchTree() : root(nullptr), numOfNodes(0) {}
+
+    void insert(T val);
+    Node<T>* search(T val) const;
+    void remove(T val);
+    T findMin() const;
+    T findMax() const;
+    int size() const;
+    void print() const;
+};
+
+template<class T>
+Node<T> * BinarySearchTree<T>::insert(Node<T> *node, T value)
+{
+    if (node == nullptr)
+        return new Node<T>(value);
+
+    if (value < node->data)
+        node->left = insert(node->left, value);
+    else if (value > node->data)
+        node->right = insert(node->right, value);
+
+    return node;
+}
+
+template<class T>
+Node<T>* BinarySearchTree<T>::search(Node<T> *node, T value) const
+{
+    if (node == nullptr)
+        return nullptr;
+    else if (value < node->data)
+        return search(node->left, value);
+    else if (value > node->data)
+        return search(node->right, value);
+    else
+        return node;
+}
+
+template<class T>
+Node<T>* BinarySearchTree<T>::remove(Node<T> *node, T value)
+{
+    if (node == nullptr)
+        return nullptr;
+    else if (value < node->data)
+        node->left = remove(node->left, value);
+    else if (value > node->data)
+        node->right = remove(node->right, value);
+    else
+    {
+        if (node->left == nullptr)
+        {
+            Node<T>* temp = node->right;
+            delete node;
+            return temp;
+        }
+        else if (node->right == nullptr)
+        {
+            Node<T>* temp = node->left;
+            delete node;
+            return temp;
+        }
+
+        else
+        {
+            Node<T>* min = findMin(node->right);
+            node->data = min->data;
+            node->right = remove(node->right, min->data);
+        }
+    }
+    return node;
+}
+
+template<class T>
+Node<T> * BinarySearchTree<T>::findMin(Node<T> *node) const
+{
+    if (node == nullptr)
+        return nullptr;
+
+    while (node->left != nullptr)
+        node = node->left;
+    return node;
+}
+
+template<class T>
+Node<T> * BinarySearchTree<T>::findMax(Node<T> *node) const
+{
+    if (node == nullptr)
+        return nullptr;
+
+    while (node->right != nullptr)
+        node = node->right;
+
+    return node;
+}
+
+template<class T>
+void BinarySearchTree<T>::print(Node<T> *node, int spaces) const
+{
+    if (node == nullptr)
+        return;
+
+    const int SPACES = 4;
+    spaces += SPACES;
+
+    //std::cout << "Test node " << node->data << " spaces lvl "<< spaces << std::endl;
+
+    print(node->right, spaces);
+
+    std::cout << std::setw(spaces) << node->data << std::endl;
+
+    print(node->left, spaces);
+}
+
+template<class T>
+void BinarySearchTree<T>::insert(T val)
+{
+    this->root = insert(this->root, val);
+    numOfNodes++;
+}
+
+template<class T>
+Node<T>* BinarySearchTree<T>::search(T val) const
+{
+    return search(this->root, val);
+}
+
+template<class T>
+void BinarySearchTree<T>::remove(T val)
+{
+    this->root = remove(this->root, val);
+    numOfNodes--;
+}
+
+template<class T>
+T BinarySearchTree<T>::findMin() const
+{
+    return findMin(root)->data;
+}
+
+template<class T>
+T BinarySearchTree<T>::findMax() const
+{
+    return findMax(root)->data;
+}
+
+template<class T>
+int BinarySearchTree<T>::size() const
+{
+    return numOfNodes;
+}
+
+template<class T>
+void BinarySearchTree<T>::print() const
+{
+    print(root,0);
+}
+
+
+#endif //BINARYSEARCHTREE_H
---
+++ _14/BST_implementation/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "BinarySearchTree.h"
+
+using namespace std;
+
+int main()
+{
+    BinarySearchTree<int> bst;
+
+    cout << "Inserting values: 10, 5, 15, 3, 7, 12, 18" << endl;
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(15);
+    bst.insert(3);
+    bst.insert(7);
+    bst.insert(12);
+    bst.insert(18);
+
+    bst.print();
+
+    cout << "--------------------------------------\n";
+    cout << "Testing search funciton\n";
+    cout << "Searching for 5: " << (bst.search(5) ? "Found" : "Not Found") << endl;
+    cout << "Searching for 50: " << (bst.search(50) ? "Found" : "Not Found") << endl;
+
+    cout << "--------------------------------------\n";
+    cout << "testing minimum and maximum functions\n";
+    cout << "Minimum value is: " << bst.findMin() << endl;
+    cout << "Maximum value is: " << bst.findMax() << endl;
+
+    cout << "--------------------------------------\n";
+    cout << "testing the size function\n";
+    cout << "Number of nodes is: " << bst.size() << endl;
+
+    cout << "---------------------------------------\n";
+    cout << "testing remove funcition\n";
+    cout << "Removing 3 (a leaf node)" << endl;
+    bst.remove(3);
+    cout << "Number of items after removing 3: " << bst.size() << endl;
+    cout << "Removing 15 (a node with one child)" << endl;
+    bst.remove(15);
+    cout << "Number of items after removing 15: " << bst.size() << endl;
+    cout << "Removing 10 (the root node)" << endl;
+    bst.remove(10);
+    cout << "Number of items after removing 10: " << bst.size() << endl;
+    bst.print();
+    
+
+    return 0;
+}
